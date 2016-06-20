50525afbc04d136140bec5cc1108ee881e2c7d76
Mulley <mullen.michael523@gmail.com>
2016-06-20 00:11:57 -0700

    Support for 'players' arg. Returning list of heroes.

--- application/resources/marvel_randomizer.py
+++ application/resources/marvel_randomizer.py
@@ -1,10 +1,33 @@
-from flask import request
-from flask_restful import Resource
+import random
+from flask_restful import abort, reqparse, Resource
+from application.common import cards
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('players', type=int)
 
 
 def generate_game(request_data):
     """Return a valid game of Marvel Legendary."""
-    return {"game": "gamessss"}
+    heroes_index = []
+    data_to_return = {'Heroes': []}
+
+    # Return Heroes
+    if not request_data['players']:
+        abort(400, message="You didn't specify the amount of players!")
+    elif request_data['players'] not in cards.PLAYER_AMOUNT:
+        abort(400, message="Invalid amount of players specified.")
+
+    if request_data['players'] != 5:
+        heroes_index.extend(
+            random.sample(range(0, len(cards.HEROES['Core'])), 5))
+    else:
+        heroes_index.extend(
+            random.sample(range(0, len(cards.HEROES['Core'])), 6))
+
+    for i in heroes_index:
+        data_to_return['Heroes'].append(cards.HEROES['Core'][i]['Name'])
+    return data_to_return
 
 
 class MarvelRandomizer(Resource):
@@ -12,5 +35,6 @@ class MarvelRandomizer(Resource):
 
     def post(self):
         """Genereate a new game."""
-        print(request.form['data'])
-        return generate_game(request.form['data'])
+        args = parser.parse_args()
+        request_data = {'players': args['players']}
+        return generate_game(request_data)
